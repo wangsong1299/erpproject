a41df715acc885ba83f81ecb293abcd36e8c51e1
wangsong <wangsong1299@163.com>
2016-01-26 23:16:12 +0800

    查询

--- statistics/urls.py
+++ statistics/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     url(r'^statistics_list/(\d{1,6})/$', views.statistics_list),
     url(r'^statistics_search/(\d{4})/(\d{1,2})/(\d{1,2})/$', views.statistics_search),
 
+    url(r'^time_search/$', views.time_search),
+    url(r'^time_search_d/(\d{8})/(\d{8})/$', views.time_search_d),
+    url(r'^time_search_f/(\d{8})/(\d{8})/$', views.time_search_f),
+
+
 )
--- statistics/views.py
+++ statistics/views.py
@@ -9,7 +9,7 @@ from work.models import Worker
 from storage.models import Storage,Storage_in,Storage_out,Storage_material
 from django.utils.timezone import now, timedelta
 from users.models import User
-
+from finance.models import Finance
 # Create your views here.
 def get_statistics_list(records):	
 	a={}
@@ -55,6 +55,40 @@ def statistics_list(request,num):
 		a=get_statistics_list(records)
 		return render_to_response("statistics_statistics.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':a,'pre_click':json.dumps(pre_click),'later_click':json.dumps(later_click)})
 
+def get_delivery_list(records):
+	a={}
+	i=0
+	for r in records:
+		b={}
+		b[0]=r.deliveryID
+		b[1]=r.product_name
+		b[2]=r.order_amount
+		b[3]=r.delivery_amount
+		b[4]=r.fee
+		b[5]=r.id
+		b[6]=r.delivery_time
+		a[i]=b
+		i=i+1
+	return a
+
+def get_finance_list(records):	
+	a={}
+	i=0
+	for r in records:
+		b={}
+		b[0]=r.delivery_time
+		b[1]=r.productID
+		b[2]=r.product_name
+		b[3]=r.order_amount
+		b[4]=r.delivery_amount
+		b[5]=r.price
+		b[6]=r.total_fee
+		b[7]=r.notes
+		b[8]=r.id
+		a[i]=b
+		i=i+1
+	return a
+
 def statistics_search(request,year,month,day):
 	is_login=request.session.get('is_login',False)
 	nick_name = request.session.get('nick_name',False)
@@ -69,4 +103,37 @@ def statistics_search(request,year,month,day):
 			a=get_statistics_list(records)
 			return render_to_response("statistics_search.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':a,'isValue':True})
 
+def time_search(request):
+	is_login=request.session.get('is_login',False)
+	nick_name = request.session.get('nick_name',False)
+	return render_to_response("statistics_time_search.html",{'is_login':json.dumps(is_login),'nick_name':nick_name})
 
+def time_search_d(request,start,end):
+	is_login=request.session.get('is_login',False)
+	nick_name = request.session.get('nick_name',False)
+	if not is_login:
+		return HttpResponseRedirect("/")
+	else:
+		startdate=start[0:4]+"-"+start[4:6]+"-"+start[6:8]
+		enddate=end[0:4]+"-"+end[4:6]+"-"+end[6:8]
+		records=Delivery.objects.filter(delivery_time__range=(startdate, enddate))
+		if (len(records)==0):
+			return render_to_response("statistics_time_search_d.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':records,'isValue':False})
+		else:
+			a=get_delivery_list(records)
+			return render_to_response("statistics_time_search_d.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':a,'isValue':True})
+
+def time_search_f(request,start,end):
+	is_login=request.session.get('is_login',False)
+	nick_name = request.session.get('nick_name',False)
+	if not is_login:
+		return HttpResponseRedirect("/")
+	else:
+		startdate=start[0:4]+"-"+start[4:6]+"-"+start[6:8]
+		enddate=end[0:4]+"-"+end[4:6]+"-"+end[6:8]
+		records=Finance.objects.filter(delivery_time__range=(startdate, enddate))
+		if (len(records)==0):
+			return render_to_response("statistics_time_search_f.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':records,'isValue':False})
+		else:
+			a=get_finance_list(records)
+			return render_to_response("statistics_time_search_f.html",{'is_login':json.dumps(is_login),'nick_name':nick_name,'records':a,'isValue':True})
